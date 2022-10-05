3c6a781c1d99965298e0ffce003754b199a0c79a
Dxdesignz <114389758+Dxdesignz@users.noreply.github.com>
2022-10-05 11:59:54 +0500

    Skiness Website

---
+++ 9/about/about.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".slider", {
+    spaceBetween: 50,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
